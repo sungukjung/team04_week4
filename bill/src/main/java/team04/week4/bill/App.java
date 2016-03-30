a727d9cf16bf42ec7d7a1f989f6bd25d3da1c952
ChoJunYoung <jjy891224@gmail.com>
2016-03-30 19:59:01 +0900

    Make Project

---
+++ bill/src/main/java/team04/week4/bill/App.java
@@ -0,0 +1,13 @@
+package team04.week4.bill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
