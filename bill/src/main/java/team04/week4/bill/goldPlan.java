b6bd00e4dd28ef604a81c4b6a23aa1aed34d3f0f
jsunwoo <jsunwoo@ualberta.ca>
2016-04-01 17:45:50 +0900

    골드플랜 추가

--- bill/src/main/java/team04/week4/bill/goldPlan.java
+++ bill/src/main/java/team04/week4/bill/goldPlan.java
@@ -1,5 +1,31 @@
 package team04.week4.bill;
 
-public class goldPlan {
+public class goldPlan implements planType {
 
+	public double basicRate = 49.95;
+	public int includedMinute = 1000;
+	public double excessRate = 0.45;
+	public double additionalLineRate = 14.5;
+	public double familyDiscountRate = 5;
+	
+	
+	public double getBasicMonthlyRate() {
+		return basicRate;
+	}
+
+	public double getExcessMinuteRate() {
+		return excessRate;
+	}
+
+	public int getIncludedMinute() {
+		return includedMinute;
+	}
+
+	public double getFamilyDiscountRate() {
+		return familyDiscountRate;
+	}
+
+	public double getAdditionalLineRate() {
+		return additionalLineRate;
+	}
 }
