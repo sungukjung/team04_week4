c8b1f50490c3b6a607ffab8a04f69ec4f72423af
ChoJunYoung <jjy891224@gmail.com>
2016-04-02 19:02:28 +0900

    fix App

--- bill/src/main/java/edu/team04/bill/App.java
+++ bill/src/main/java/edu/team04/bill/App.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
  *
  */
 public class App {
-<<<<<<< HEAD:bill/src/main/java/team04/week4/bill/App.java
 	private App() {
 
 	}
@@ -54,45 +53,4 @@ public class App {
 			out.printf("your total fee is %.2f%n", totalFee);
 		}
 	}
-=======
-    public static void main( String[] args ) {
-        Scanner scan = new Scanner(System.in); 
-        String planType, minUsage, numberOfLines;
-        int min, lines;
-        double totalFee;
-        Calculator cal;
-        PrintStream out = System.out;
-       
-        while(true){
-        	out.println("input your plan type(exit is 0)");
-        	
-        	planType = scan.nextLine();
-        	if ( "0".equals(planType) ){
-        		break;
-        	}
-        	
-        	out.println("your plan type is "+planType);
-        	
-        	out.println("input your usage(minutes)");
-        	
-        	minUsage = scan.nextLine();
-        	min = Integer.parseInt(minUsage);
-        	
-        	out.println("your plan usage is "+minUsage+ " minutes");
-        	
-        	out.println("input your number of lines");
-        	
-        	numberOfLines = scan.nextLine();
-        	lines = Integer.parseInt(numberOfLines);
-        	
-        	out.println("your line is "+numberOfLines);
-        	
-        	cal = new Calculator(lines, min, planType);
-        	
-        	totalFee = cal.calculateTotalRate();
-        	
-        	out.printf("your total fee is %.2f%n",totalFee);
-        }        
-    }
->>>>>>> 8127a0e109ca7533199282fb45b8d8b5b796bbad:bill/src/main/java/edu/team04/bill/App.java
 }
