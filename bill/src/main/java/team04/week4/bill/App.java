27d49206b6a632d18bf73373ebc13eb1aa69e028
ChoJunYoung <jjy891224@gmail.com>
2016-04-02 17:09:38 +0900

    reset

--- bill/src/main/java/team04/week4/bill/App.java
+++ bill/src/main/java/team04/week4/bill/App.java
@@ -1,5 +1,8 @@
 package team04.week4.bill;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,42 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scan = new Scanner(System.in);
+        String planType, minUsage, numberOfLines;
+        int min, lines;
+        double totalFee;
+        Calculator cal;
+        PrintStream out = System.out;
+        
+        
+        while(true){
+        	out.println("input your plan type(exit is 0)");
+        	
+        	planType = scan.nextLine();
+        	if ( "0".equals(planType) )
+        		break;
+        	
+        	out.println("your plan type is "+planType);
+        	
+        	out.println("input your usage(minutes)");
+        	
+        	minUsage = scan.nextLine();
+        	min = Integer.parseInt(minUsage);
+        	
+        	out.println("your plan usage is "+minUsage+ " minutes");
+        	
+        	out.println("input your number of lines");
+        	
+        	numberOfLines = scan.nextLine();
+        	lines = Integer.parseInt(numberOfLines);
+        	
+        	out.println("your line is "+numberOfLines);
+        	
+        	cal = new Calculator(lines, min, planType);
+        	
+        	totalFee = cal.calculateTotalRate();
+        	
+        	out.printf("your total fee is %.2f%n",totalFee);
+        }        
     }
 }
--- bill/src/main/java/team04/week4/bill/Calculator.java
+++ bill/src/main/java/team04/week4/bill/Calculator.java
@@ -3,12 +3,12 @@ package team04.week4.bill;
 public class Calculator {
 	private planType userPlanType;
 	private User user;
-
+	private double ExceedRate = 0;
 	private double billCharged;
 
 	Calculator(int line, int min, String plan) {
 		user = new User(line, min, plan);
-		if (plan.equals("silver")) {
+		if ("silver".equals(plan)) {
 			userPlanType = new silverPlan();
 		}
 
@@ -17,31 +17,36 @@ public class Calculator {
 		}
 	}
 
-
 	private double calculateBasicRate() {
-			double BasicRate = userPlanType.getBasicMonthlyRate();
-			return BasicRate;
-		}
+		double BasicRate = userPlanType.getBasicMonthlyRate();
+		return BasicRate;
+	}
 
 	private double calculateAdditionalRate() {
-			double numberOfLine = user.line;
-			double additionalRate;
-			
-			if(numberOfLine >= 4) {
-				additionalRate = 2 * userPlanType.getAdditionalLineRate() + 5 * (user.line-3);
-			}
-			else {
-				additionalRate = (user.line-1) * userPlanType.getAdditionalLineRate();
-			}
-			
-			return additionalRate;
+		double numberOfLine = user.line;
+		double additionalRate;
+
+		if (numberOfLine >= 4) {
+			additionalRate = 2 * userPlanType.getAdditionalLineRate() + 5 * (user.line - 3);
+		} else {
+			additionalRate = (user.line - 1) * userPlanType.getAdditionalLineRate();
 		}
 
+		return additionalRate;
+	}
+
 	private double calculateExceededRate() {
+		int ExceedMinute = user.min - userPlanType.getIncludedMinute();
+
+		if (0 < ExceedMinute) {
+
+			ExceedRate = userPlanType.getExcessMinuteRate() * ExceedMinute;
 		}
-	
+		return ExceedRate;
+	}
+
 	public double calculateTotalRate(){
-		billCharged = 
+		billCharged = calculateBasicRate()+calculateAdditionalRate()+calculateExceededRate();
 		return billCharged;
 	}
 
